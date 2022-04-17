88b479141de9405857e6cd4cd1926a566689af5d
Tien Hyun <tizzyt566@gmail.com>
2022-04-17 17:01:24 -0400

    Ensure duplicate column names arent possible

--- BijouDB/TableBuilder.cs
+++ BijouDB/TableBuilder.cs
@@ -1,4 +1,5 @@
-﻿using BijouDB.Primitives;
+﻿using BijouDB.Exceptions;
+using BijouDB.Primitives;
 
 namespace BijouDB;
 
@@ -7,11 +8,14 @@ public class TableBuilder
     private long _offset = 0;
     private int _count = 0;
 
+    private readonly HashSet<string> _columnNames = new();
+
     public TableBuilder Add<T>(out Column<T> column, ColumnType attribute) where T : IDataType, new() => Add(out column, null, attribute);
     public TableBuilder Add<T>(out Column<T> column, string? columnName = null, ColumnType type = ColumnType.None) where T : IDataType, new()
     {
-        if (columnName is not null) Misc.EnsureAlphaNumeric(columnName, true);
+        if (columnName is not null) Misc.EnsureAlphaNumeric(columnName);
         columnName = $"{Globals.ColName}_{columnName ?? _count.ToString()}";
+        if (!_columnNames.Add(columnName)) throw new DuplicateColumnException(columnName);
         column = new(columnName, _offset, type);
         _offset += T.Length;
         _count++;
