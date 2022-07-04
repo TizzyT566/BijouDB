a15f6baba1560e71ac5722c9fc222cca0963c5ea
Tien Hyun <tizzyt566@gmail.com>
2022-07-04 00:46:26 -0400

    typo

--- BijouDB/Components/Cache.cs
+++ BijouDB/Components/Cache.cs
@@ -7,7 +7,7 @@ internal class Cache<D> where D : IDataType
     private Node? _head, _tail;
     private int _lock;
 
-    public Cache(int count) => count = count > 0 ? count : throw new InvalidOperationException("Unexpected Error, Cache must be greater than 0.");
+    public Cache(int count) => _count = count > 0 ? count : throw new InvalidOperationException("Unexpected Error, Cache must be greater than 0.");
 
     internal void Set(Guid key, D? value)
     {
